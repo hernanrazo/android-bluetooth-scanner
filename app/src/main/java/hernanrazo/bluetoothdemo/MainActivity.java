bf5500e9d9f5fa5542e9c29c5d844563d817aad7
Hernan Razo <hernanrazo@gmail.com>
2018-07-20 01:11:24 -0500

    worked on location permission

--- app/src/main/java/hernanrazo/bluetoothdemo/MainActivity.java
+++ app/src/main/java/hernanrazo/bluetoothdemo/MainActivity.java
@@ -1,8 +1,12 @@
 package hernanrazo.bluetoothdemo;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,12 +14,48 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int BLUETOOTH_REQUEST = 1;
-    private int LOCATION_REQUEST = 2;
+    private final int BLUETOOTH_REQUEST = 1;
+    private final int LOCATION_REQUEST = 2;
 
     //TODO: request location permission
 
+    public void locationPermission() {
 
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                new AlertDialog.Builder(this).setTitle("Locations Permission Needed")
+                        .setMessage("Locations permission needed to continue");
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_REQUEST);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode,
+                                          String permissions[],
+                                          int[] grantResults) {
+
+        switch(requestCode) {
+
+            case LOCATION_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast.makeText(this,
+                            "Locations permission needed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +84,9 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent btEnableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(btEnableIntent, BLUETOOTH_REQUEST);
-
             }
         }
+        locationPermission();
     }
 
 
